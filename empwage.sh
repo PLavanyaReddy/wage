e7080d3208ad39498eb2837ee130ed64c1cc2511
unknown <lavanyareddy19994@gmail.com>
2020-07-14 16:17:51 +0530

    employee attendance

--- empwage.sh
+++ empwage.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo "Welcome to employee wage calculation"
+ispresent=$(( RANDOM%2 ))
+if [ $ispresent -eq 1 ]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
